48064d75e7341c08a96f272d3296786cbbcdf695
ShraddhaWath2002 <161481320+ShraddhaWath2002@users.noreply.github.com>
2024-07-28 21:40:25 +0530

    Delete nt directory

--- nt/dao/IGuestDAO.java
+++
@@ -1,7 +0,0 @@
-package com.nt.dao;
-
-import com.nt.bo.GuestBO;
-
-public interface IGuestDAO {
-	public int insert(GuestBO gbo)throws Exception;
-}
--- nt/service/IGuestMgmtService.java
+++
@@ -1,7 +0,0 @@
-package com.nt.service;
-
-import com.nt.dto.GuestDTO;
-
-public interface IGuestMgmtService {
-	public String registerGuest(GuestDTO dto)throws Exception;
-}
